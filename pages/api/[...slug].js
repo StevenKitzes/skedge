0c9c08b63c1885d3bbcb782a758faf58360e425d
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-03 23:17:07 -0700

    add sloppy example of writing to dynamo from api

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -1,7 +1,34 @@
+const aws = require('aws-sdk')
+
+aws.config.update({
+  region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT,
+  table: process.env.AWS_TABLE,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+})
+
+const dynamoClient = new aws.DynamoDB.DocumentClient();
+
 function handler(req, res) {
   const {slug} = req.query
   const [eventId, userId] = slug
-  res.end(`Got eventId: ${eventId} and userId: ${userId}`)
+
+  const dynamoPutParams = {
+    TableName: process.env.AWS_TABLE,
+    Item: {
+      guid: 'test', eventId, userId
+    }
+  }
+  dynamoClient.put(dynamoPutParams, (err) => {
+    if (err) {
+        console.log(`db error: ${JSON.stringify(err, null, 2)}`);
+    } else {
+        console.log(`db success`);
+    }
+  });
+
+  res.end(`created dynamo record (probably)`)
 }
 
 export default handler
