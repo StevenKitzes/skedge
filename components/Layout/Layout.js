11c2af887fa1b94d3446bf8f68d800faba33d7b3
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-07 20:09:19 -0700

    add footer and some supporting styles

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,3 +1,4 @@
+import Footer from '../Footer'
 import Header from '../Header'
 import styles from './Layout.module.scss'
 
@@ -6,6 +7,7 @@ function Layout({ children }) {
     <div className={styles.container}>
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }
