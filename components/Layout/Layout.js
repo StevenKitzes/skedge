2727bc15d4641307e80ff6afd357da3c1cb30e11
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-07 18:58:05 -0700

    add header and related assets

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,10 @@
+import Header from '../Header'
 import styles from './Layout.module.scss'
 
 function Layout({ children }) {
   return (
     <div className={styles.container}>
+      <Header />
       {children}
     </div>
   )
