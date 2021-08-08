f9288470f694e3d013b0501c72fb70754b154850
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-08 00:09:45 -0700

    attempt fix for false hover event on main page button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Button from '../components/Button'
 import Layout from '../components/Layout'
 import styles from './index.module.scss'
 
 export default function Home() {
+  useEffect(() => {
+    document.getElementById('title').focus();
+  })
   return (
     <Layout>
       <Head>
@@ -11,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.titleContainer}>
+      <div className={styles.titleContainer} id='title'>
         <h1 className={styles.welcomeTo}>Welcome to</h1>
         <img className={styles.skedgeLogo} src='../images/skedge-logo-outline.svg' />
       </div>
