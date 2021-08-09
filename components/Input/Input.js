ab36fdbcb674f10ace799181528ff58ecbf48422
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-08 17:51:53 -0700

    add multiline option to input component

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,14 +1,21 @@
 import clsx from 'clsx'
 import styles from './Input.module.scss'
 
-function Input({ errorMessage, invalid = false, label, onChange }) {
+function Input({ errorMessage, invalid = false, label, multiline = false, onChange }) {
   return (
     <div className={styles.inputContainer}>
       <p className={styles.label}>{label}</p>
-      <input
-        className={clsx(styles.input, invalid && styles.inputError)}
-        onChange={onChange}
-      />
+      {
+        multiline
+          ? <textarea
+              className={clsx(styles.input, invalid && styles.inputError)}
+              onChange={onChange}
+            />
+          : <input
+              className={clsx(styles.input, invalid && styles.inputError)}
+              onChange={onChange}
+            />
+      }
       <p className={clsx(styles.errorMessage, invalid && styles.errorMessageVisible)}>
         {errorMessage}
       </p>
