1b68e65bd3a5a349af100b632b345ac97898c6b7
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-15 14:46:58 -0700

    switch to unreserved environment var names for netlify

--- helpers/db.js
+++ helpers/db.js
@@ -1,20 +1,20 @@
 const aws = require('aws-sdk')
 
 aws.config.update({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT,
-  table: process.env.AWS_EVENTS_TABLE,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+  region: process.env.SKEDGE_AWS_REGION,
+  endpoint: process.env.SKEDGE_AWS_ENDPOINT,
+  table: process.env.SKEDGE_AWS_EVENTS_TABLE,
+  accessKeyId: process.env.SKEDGE_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.SKEDGE_AWS_SECRET_KEY
 })
 const eventsClient = new aws.DynamoDB.DocumentClient()
 
 aws.config.update({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.AWS_ENDPOINT,
-  table: process.env.AWS_USERS_TABLE,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+  region: process.env.SKEDGE_AWS_REGION,
+  endpoint: process.env.SKEDGE_AWS_ENDPOINT,
+  table: process.env.SKEDGE_AWS_USERS_TABLE,
+  accessKeyId: process.env.SKEDGE_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.SKEDGE_AWS_SECRET_KEY
 })
 const usersClient = new aws.DynamoDB.DocumentClient()
 
@@ -34,13 +34,13 @@ function dynamoPutParams (table, data) {
 // callback should be of the form (err, data) => { ... }
 async function readEvent (eventId, callback) {
   eventsClient.get(
-    dynamoGetParams(process.env.AWS_EVENTS_TABLE, {guid: eventId}),
+    dynamoGetParams(process.env.SKEDGE_AWS_EVENTS_TABLE, {guid: eventId}),
     callback
   )
 }
 async function readUser (eventId, userId, callback) {
   usersClient.get(
-    dynamoGetParams(process.env.AWS_USERS_TABLE, {eventId, userId}),
+    dynamoGetParams(process.env.SKEDGE_AWS_USERS_TABLE, {eventId, userId}),
     callback
   )
 }
@@ -48,13 +48,13 @@ async function readUser (eventId, userId, callback) {
 // callback should be of the form (err) => { ... }
 async function writeEvent (data, callback) {
   eventsClient.put(
-    dynamoPutParams(process.env.AWS_EVENTS_TABLE, data),
+    dynamoPutParams(process.env.SKEDGE_AWS_EVENTS_TABLE, data),
     callback
   )
 }
 async function writeUser (data, callback) {
   usersClient.put(
-    dynamoPutParams(process.env.AWS_USERS_TABLE, data),
+    dynamoPutParams(process.env.SKEDGE_AWS_USERS_TABLE, data),
     callback
   )
 }
