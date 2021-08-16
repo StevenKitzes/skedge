753175110fbdd75d225a53558d45919f9dca4a49
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-15 20:02:48 -0700

    clean up tech debt

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,16 @@
 import Button from '../components/Button'
+import Hero from '../components/Hero'
 import Layout from '../components/Layout'
+import Oof from '../components/Oof'
 import Separator from '../components/Separator'
-import styles from './404.module.scss'
 
 function FourOhFour() {
   return (
     <Layout>
-      <h1 className={styles.title}>404</h1>
-      <h2>No such page!</h2>
+      <Hero title='Page Not Found' />
       <Separator />
-      <p className={styles.question}>Did you try to schedule something on another timeline?</p>
+      <Oof copy='Did you try to schedule something on another timeline?' />
       <Button
-        classes={styles.button}
         href='/'
         label='Back to Skedge'
       />
