26152132a6e6e77ff134970b6de871c3f8404189
Steven Kitzes <steven.kitzes@gmail.com>
2021-08-16 01:20:56 -0700

    adapt api slug to accept event and event plus user requests

--- pages/api/[...slug].js
+++ pages/api/[...slug].js
@@ -12,14 +12,6 @@ async function handler(req, res) {
       resolve(data.Item)
     })
   })
-  const readUserPromise = new Promise((resolve, reject) => {
-    db.readUser(eventId, userId, (err, data) => {
-      if (err) {
-        reject(`Error reading User database: ${JSON.stringify(err)}`)
-      }
-      resolve(data.Item)
-    })
-  })
   const queryUsersPromise = new Promise((resolve, reject) => {
     db.queryEventUsers(eventId, (err, data) => {
       if (err) {
@@ -29,9 +21,24 @@ async function handler(req, res) {
     })
   })
 
-  await Promise.all([readEventPromise, readUserPromise, queryUsersPromise])
-    .then((values) => res.status(200).end(JSON.stringify(values)))
-    .catch((err) => res.status(500).end(JSON.stringify(err)))
+  if (userId) {
+    const readUserPromise = new Promise((resolve, reject) => {
+      db.readUser(eventId, userId, (err, data) => {
+        if (err) {
+          reject(`Error reading User database: ${JSON.stringify(err)}`)
+        }
+        resolve(data.Item)
+      })
+    })
+    await Promise.all([readEventPromise, readUserPromise, queryUsersPromise])
+      .then((values) => res.status(200).end(JSON.stringify(values)))
+      .catch((err) => res.status(500).end(JSON.stringify(err)))
+    return
+  }
+
+  await Promise.all([readEventPromise, queryUsersPromise])
+  .then((values) => res.status(200).end(JSON.stringify(values)))
+  .catch((err) => res.status(500).end(JSON.stringify(err)))
 }
 
 export default handler
